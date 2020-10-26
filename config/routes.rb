ecaed808e124b9b3bf5ddca0def4c166e4bf9485
Shekhar-Patil <patilshekhar900@gmail.com>
2020-10-26 09:50:22 +0530

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   devise_for :users, skip: [:registrations], controllers: { sessions: 'sessions' }
 
   devise_scope :users do
     devise_for :users, path: 'customers', class_name: 'User', only: %i[registrations], controllers: { registrations: 'customers/users' }
-
   end
 
   devise_scope :users do
